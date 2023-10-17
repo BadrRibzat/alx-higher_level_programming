05ab29f5589d30dfbc0b3e81fa6789fee200e122
BadrRibzat <badrribzat@gmail.com>
2023-10-17 07:44:00 -0700

    Temperatures

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,2 @@
+-- a Script  that lists the number of records with the same score in the table second_table
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER by score DESC;
